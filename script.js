d951d0e7884b8dc6431b3cc067a89d136f966df6
Juan <juan.barezzi@gmail.com>
2022-04-03 14:06:56 -0700

    Implementado a seleção do pedido no aplicativo

--- script.js
+++ script.js
@@ -1,5 +1,57 @@
-function select() {
-    const order = document.querySelector(".product");
-    order.classList.toggle("selected-box");
-    order.children[4].classList.toggle("hidden")
+let food, drink, dessert;
+
+function selectFood(foodBox) {
+
+    const selectedFood =  document
+        .querySelector(".food")
+        .querySelector(".selected-box");
+    if (selectedFood !== null) {
+        selectedFood.classList.remove("selected-box");
+        selectedFood.children[4].classList.add("hidden");
+    }
+
+    foodBox.classList.add("selected-box");
+    foodBox.children[4].classList.remove("hidden");
+
+    food = foodBox;
+}
+
+function selectDrink(drinkBox) {
+
+    const selectedDrink =  document
+        .querySelector(".drink")
+        .querySelector(".selected-box");
+    if (selectedDrink !== null) {
+        selectedDrink.classList.remove("selected-box");
+        selectedDrink.children[4].classList.add("hidden");
+    }
+
+    drinkBox.classList.add("selected-box");
+    drinkBox.children[4].classList.remove("hidden");
+
+    drink = drinkBox;
+}
+
+function selectDessert(dessertBox) {
+
+    const selectedDessert =  document
+        .querySelector(".dessert")
+        .querySelector(".selected-box");
+    if (selectedDessert !== null) {
+        selectedDessert.classList.remove("selected-box");
+        selectedDessert.children[4].classList.add("hidden");
+    }
+
+    dessertBox.classList.add("selected-box");
+    dessertBox.children[4].classList.remove("hidden");
+
+    dessert = dessertBox;
+}
+
+function order() {
+    if ( food && drink && dessert) {
+        const disabledButton = document.querySelector("button")
+        disabledButton.toggleAttribute("disabled")
+        alert("SIM")
+    }
 }
